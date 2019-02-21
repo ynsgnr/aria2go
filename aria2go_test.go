74449c3b17d15f53fe4a956a4aa7d423bef786ea
Yunus Güngör <yunusgnr@gmail.com>
2019-02-21 13:22:15 +0100

    added test cases for adduri and gid hex conversitons

--- aria2go_test.go
+++ aria2go_test.go
@@ -1,10 +1,12 @@
 package aria2
 
 import "testing"
-//import "gotest.tools/assert"
+import "gotest.tools/assert"
 
 var downloader Downloader
 var session Session
+var gid Gid
+var gid_position Gid
 
 func TestMain(t *testing.M){
 	downloader = New()
@@ -17,4 +19,30 @@ func TestSession(t *testing.T){
 
 func TestRun(t *testing.T){
 	downloader.run(session)
+}
+
+func TestAddUri(t *testing.T){
+	//Todo maybe check files md5 with argon2
+	gid = downloader.addUri("https://www.w3.org/History/1989/Image1.gif") //oldest file in the internet, unlikely to be deleted
+}
+
+func TestAddUriInPosition(t *testing.T){
+	//Todo maybe check files md5 with argon2
+	gid_position = downloader.addUriInPosition("https://www.w3.org/History/1989/Image2.gif",0) //oldest file in the internet, unlikely to be deleted
+	//Todo add get current position and check it
+}
+
+func TestisNull(t *testing.T){
+	assert.Equal(t,downloader.isNull(gid),true)
+}
+
+func TestGidToHex(t *testing.T){
+	downloader.gidToHex(gid)
+	//hex can be anything no way to check
+}
+
+func TestHexToGid(t *testing.T){
+	hex := downloader.gidToHex(gid)
+	gid_converted := downloader.hexToGid(hex)
+	assert.Equal(t,gid,gid_converted)
 }
