389a21555401889608a4ce04aa809853e2efe279
Yunus Güngör <yunusgnr@gmail.com>
2019-02-26 08:24:30 +0100

    Added more tests and increased stability of tests

--- aria2go_test.go
+++ aria2go_test.go
@@ -3,8 +3,8 @@ package aria2
 import "testing"
 import "gotest.tools/assert"
 import "os"
-
 import "fmt"
+import "time"
 
 var downloader aria2go
 var gid Gid
@@ -21,25 +21,27 @@ var error_count = 0
 func TestMain(m *testing.M){
 	downloader = New()
 	downloader.setEventCallback(func(event DownloadEvent,g Gid){
-		fmt.Printf("Callaback Called:\n")
+		fmt.Printf("Callback Called:\n")
+		h := downloader.getDownloadHandle(g)
+		s := h.getStatus()
 		switch event {
 		case EVENT_ON_DOWNLOAD_START:
-			fmt.Printf("Download Start\n")
+			fmt.Printf("Download Start: %s\n",s)
 			start_count++
 		case EVENT_ON_DOWNLOAD_PAUSE:
-			fmt.Printf("Download Pause\n")
+			fmt.Printf("Download Pause: %s\n",s)
 			pause_coount++
 		case EVENT_ON_DOWNLOAD_STOP:
-			fmt.Printf("Download Stop\n")
+			fmt.Printf("Download Stop: %s\n",s)
 			stop_count++
 		case EVENT_ON_DOWNLOAD_COMPLETE:
-			fmt.Printf("Download Complete\n")
+			fmt.Printf("Download Complete: %s\n",s)
 			complete_count++
 		case EVENT_ON_BT_DOWNLOAD_COMPLETE:
-			fmt.Printf("Download BT Complete\n")
+			fmt.Printf("Download BT Complete: %s\n",s)
 			BT_complete_count++
 		default:
-			fmt.Printf("Download Error\n")
+			fmt.Printf("Download Error: %s\n",s)
 			error_count++
 		}
 		})
@@ -49,18 +51,40 @@ func TestMain(m *testing.M){
 }
 
 func TestAll(t *testing.T){
+	t.Run("Check if function calls protected against nullptr",func(t *testing.T){
+		//Since session not initilized, expected case is nothing to be happen
+		//If test crashes on this part check pointers and add protection
+		downloader.addUri("https://www.w3.org/History/1989/Image1.gif") //oldest file in the internet, unlikely to be deleted
+		g := downloader.addUriInPosition("https://www.w3.org/History/1989/Image2.gif",0) //oldest file in the internet, unlikely to be deleted
+		downloader.isNull(g)
+		h := downloader.gidToHex(g)
+		downloader.hexToGid(h)
+		downloader.getActiveDownload()
+		downloader.removeDownload(g)
+		downloader.forceRemoveDownload(g)
+		downloader.pauseDownload(g)
+		downloader.unpauseDownload(g)
+		downloader.forcePauseDownload(g)
+		downloader.unpauseDownload(g)
+	})
+	t.Run("Init Session",func(t *testing.T){
+		downloader.init_aria2go_session(true)
+	})
+	t.Run("Init Session Second time",func(t *testing.T){
+		downloader.init_aria2go_session(false)
+	})
+	t.Run("Run",func(t *testing.T){
+		downloader.keepRunning()
+	})
 	t.Run("Add uri",func(t *testing.T){
 		//Todo maybe check files md5 with argon2
 		gid = downloader.addUri("https://www.w3.org/History/1989/Image1.gif") //oldest file in the internet, unlikely to be deleted
-	})
-	t.Run("Check Download Status",func(t *testing.T){
-		h:=downloader.getDownloadHandle(gid)
-		s:=h.getStatus()
-		assert.Equal(t,s,"waiting")
+		time.Sleep(2 * time.Second)
 	})
 	t.Run("Add uri in position",func(t *testing.T){
 		//Todo maybe check files md5 with argon2
 		gid_position = downloader.addUriInPosition("https://www.w3.org/History/1989/Image2.gif",0) //oldest file in the internet, unlikely to be deleted
+		time.Sleep(2 * time.Second)
 		//Todo add get current position and check it		
 	})
 	t.Run("is gid null",func(t *testing.T){		
@@ -78,15 +102,12 @@ func TestAll(t *testing.T){
 	t.Run("Check Status in position",func(t *testing.T){
 		hp:=downloader.getDownloadHandle(gid_position)
 		sp:=hp.getStatus()
-		assert.Equal(t,sp,"waiting")
+		assert.Equal(t,sp,DOWNLOAD_COMPLETE)
 	})
-	t.Run("Check Run Untill Finished",func(t* testing.T){
-		hp:=downloader.getDownloadHandle(gid_position)
+	t.Run("Check Status",func(t* testing.T){
+		hp:=downloader.getDownloadHandle(gid)
 		sp:=hp.getStatus()
-		assert.Equal(t,sp,"finished")
-		h:=downloader.getDownloadHandle(gid_position)
-		s:=h.getStatus()
-		assert.Equal(t,s,"finished")
+		assert.Equal(t,sp,DOWNLOAD_COMPLETE)
 	})
 	t.Run("Check Event Counts",func(t* testing.T){
 		assert.Equal(t,start_count,2)
@@ -101,7 +122,7 @@ func TestAll(t *testing.T){
 	t.Run("Add All From Cache",func(t *testing.T){
 		downloader.addUriToCache("https://www.w3.org/History/1989/Image3.gif")
 		downloader.addUriToCache("https://www.w3.org/History/1989/proposal-magnify.gif")
-		downloader.addAllFromCache()
+		gid = downloader.addAllFromCache()
 	})
 	t.Run("Get Active Download",func(t *testing.T){
 		downloader.getActiveDownload()
