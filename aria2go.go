a7f7f668d7291fc39f4318f4aee39227ae8907e9
Yunus Güngör <yunusgnr@gmail.com>
2019-03-10 10:10:24 +0100

    Exported functions and variables

--- aria2go.go
+++ aria2go.go
@@ -45,7 +45,7 @@ func (ds DownloadStatus) String() string {
 	return names[ds]
 }
 
-type aria2go struct {
+type Aria2go struct {
 }
 
 type Gid struct {
@@ -53,39 +53,39 @@ type Gid struct {
 }
 
 type FileData struct {
-	index           int
-	path            string
-	length          int64
-	completedLength int64
-	selected        bool
+	Index           int
+	Path            string
+	Length          int64
+	CompletedLength int64
+	Selected        bool
 	//uris []string
 }
 
 type GlobalStat struct {
-	downloadSpeed int
-	uploadSpeed   int
-	numActive     int
-	numWaiting    int
-	numStopped    int
+	DownloadSpeed int
+	UploadSpeed   int
+	NumActive     int
+	NumWaiting    int
+	NumStopped    int
 }
 
 type BtMetaInfoData struct {
 	//announce list not implemented
-	comment      string
-	creationDate time.Time
-	multiMod     bool
-	singleMod    bool
-	name         string
-	valid        bool
+	Comment      string
+	CreationDate time.Time
+	MultiMod     bool
+	SingleMod    bool
+	Name         string
+	Valid        bool
 }
 
-func New() aria2go {
-	var ret aria2go
+func New() Aria2go {
+	var ret Aria2go
 	C.init_aria2go()
 	return ret
 }
 
-func (d aria2go) init_aria2go_session(keepRunning bool) {
+func (d Aria2go) Init_aria2go_session(keepRunning bool) {
 	C.finalize_aria2go()
 	if keepRunning {
 		C.init_aria2go_session(C.int(1))
@@ -94,16 +94,16 @@ func (d aria2go) init_aria2go_session(keepRunning bool) {
 	}
 }
 
-func (d aria2go) run() int {
+func (d Aria2go) Run() int {
 	return int(C.run_aria2go(C.int(0)))
 }
 
-func (d aria2go) runUntillFinished() {
+func (d Aria2go) RunUntillFinished() {
 	//Returns when all downloads finisged
 	C.keepruning_aria2go()
 }
 
-func (d aria2go) keepRunning() {
+func (d Aria2go) KeepRunning() {
 	C.finalize_aria2go()
 	C.init_aria2go_session(C.int(1))
 	go func() {
@@ -111,43 +111,43 @@ func (d aria2go) keepRunning() {
 	}()
 }
 
-func (d aria2go) runOnce() int {
+func (d Aria2go) RunOnce() int {
 	return int(C.run_aria2go(C.int(1)))
 }
 
-func (d aria2go) gidToHex(gid Gid) string {
+func (d Aria2go) GidToHex(gid Gid) string {
 	p := C.gidToHex_aria2go(gid.ptr)
 	s := C.GoString(p)
 	C.free(unsafe.Pointer(p))
 	return s
 }
 
-func (d aria2go) hexToGid(s string) Gid {
+func (d Aria2go) HexToGid(s string) Gid {
 	var ret Gid
 	ret.ptr = C.hexToGid_aria2go(C.CString(s))
 	return ret
 }
 
-func (d aria2go) isNull(g Gid) bool {
+func (d Aria2go) IsNull(g Gid) bool {
 	if C.isNull_aria2go(g.ptr) == 0 {
 		return false
 	}
 	return true
 }
 
-func (d aria2go) addUriInPosition(uri string, position int) Gid {
+func (d Aria2go) AddUriInPosition(uri string, position int) Gid {
 	var ret Gid
 	ret.ptr = C.addUri_aria2go(C.CString(uri), C.int(position))
 	return ret
 }
 
-func (d aria2go) addUri(uri string) Gid {
+func (d Aria2go) AddUri(uri string) Gid {
 	var ret Gid
 	ret.ptr = C.addUri_aria2go(C.CString(uri), C.int(-1))
 	return ret
 }
 
-func (d aria2go) addMetalinkInPosition(file_location string, position int) []Gid {
+func (d Aria2go) AddMetalinkInPosition(file_location string, position int) []Gid {
 	var gids []Gid
 	var gid Gid
 	l := int(C.addMetalink_aria2go(C.CString(file_location), C.int(position)))
@@ -158,31 +158,31 @@ func (d aria2go) addMetalinkInPosition(file_location string, position int) []Gid
 	return gids
 }
 
-func (d aria2go) addMetaLink(file_location string) []Gid {
-	return d.addMetalinkInPosition(file_location, -1)
+func (d Aria2go) AddMetaLink(file_location string) []Gid {
+	return d.AddMetalinkInPosition(file_location, -1)
 }
 
-func (d aria2go) addUriToCache(uri string) {
+func (d Aria2go) AddUriToCache(uri string) {
 	//You can use this one even if you didnt start session yet
 	C.add_uri(C.CString(uri))
 }
 
-func (d aria2go) clearUriCache() {
+func (d Aria2go) ClearUriCache() {
 	C.clear_uris()
 }
 
-func (d aria2go) addAllFromCacheWithPosition(p int) Gid {
+func (d Aria2go) AddAllFromCacheWithPosition(p int) Gid {
 	var ret Gid
 	ret.ptr = C.add_all_from_cache(C.int(p))
 	return ret
 }
-func (d aria2go) addAllFromCache() Gid {
+func (d Aria2go) AddAllFromCache() Gid {
 	var ret Gid
 	ret.ptr = C.add_all_from_cache(C.int(-1))
 	return ret
 }
 
-func (d aria2go) getActiveDownload() []Gid {
+func (d Aria2go) GetActiveDownload() []Gid {
 	var gids []Gid
 	l := int(C.getActiveDownload_aria2go())
 	for i := 0; i < l; i++ {
@@ -193,25 +193,25 @@ func (d aria2go) getActiveDownload() []Gid {
 	return gids
 }
 
-func (d aria2go) removeDownload(g Gid) {
+func (d Aria2go) RemoveDownload(g Gid) {
 	C.removeDownload_aria2go(g.ptr, C.int(0))
 	//Second variable is bool for forcing
 }
 
-func (d aria2go) forceRemoveDownload(g Gid) {
+func (d Aria2go) ForceRemoveDownload(g Gid) {
 	C.removeDownload_aria2go(g.ptr, C.int(1))
 }
 
-func (d aria2go) pauseDownload(g Gid) {
+func (d Aria2go) PauseDownload(g Gid) {
 	C.pauseDownload_aria2go(g.ptr, C.int(0))
 	//Second variable is bool for forcing
 }
 
-func (d aria2go) forcePauseDownload(g Gid) {
+func (d Aria2go) ForcePauseDownload(g Gid) {
 	C.pauseDownload_aria2go(g.ptr, C.int(1))
 }
 
-func (d aria2go) unpauseDownload(g Gid) {
+func (d Aria2go) UnpauseDownload(g Gid) {
 	C.unpauseDownload_aria2go(g.ptr)
 }
 
@@ -220,7 +220,7 @@ type EventCallback func(DownloadEvent, Gid)
 
 var callback EventCallback //Have to be global due to garbage collector
 
-func (d aria2go) setEventCallback(eventCallback EventCallback) {
+func (d Aria2go) SetEventCallback(eventCallback EventCallback) {
 	callback = eventCallback
 }
 
@@ -234,96 +234,96 @@ func runGoCallBack(event C.enum_DownloadEvent, g unsafe.Pointer) {
 	callback(DownloadEvent(event), gid)
 }
 
-func (d aria2go) finalize() {
+func (d Aria2go) Finalize() {
 	C.finalize_aria2go()
 	C.deinit_aria2go()
 }
 
-func (g Gid) getStatus() DownloadStatus {
+func (g Gid) GetStatus() DownloadStatus {
 	return DownloadStatus(C.getStatus_gid(g.ptr))
 }
 
-func (g Gid) getTotalLength() int64 {
+func (g Gid) GetTotalLength() int64 {
 	return int64(C.getTotalLength_gid(g.ptr))
 }
 
-func (g Gid) getCompletedLength() int64 {
+func (g Gid) GetCompletedLength() int64 {
 	return int64(C.getCompletedLength_gid(g.ptr))
 }
 
-func (g Gid) getUploadLength() int64 {
+func (g Gid) GetUploadLength() int64 {
 	return int64(C.getUploadLength_gid(g.ptr))
 }
 
-func (g Gid) getBitfield() string {
+func (g Gid) GetBitfield() string {
 	p := C.getBitfield_gid(g.ptr)
 	s := C.GoString(p)
 	C.free(unsafe.Pointer(p))
 	return s
 }
 
-func (g Gid) getDownloadSpeed() int {
+func (g Gid) GetDownloadSpeed() int {
 	return int(C.getDownloadSpeed_gid(g.ptr))
 }
 
-func (g Gid) getUploadSpeed() int {
+func (g Gid) GetUploadSpeed() int {
 	return int(C.getUploadSpeed_gid(g.ptr))
 }
 
-func (g Gid) getInfoHash() string {
+func (g Gid) GetInfoHash() string {
 	p := C.getInfoHash_gid(g.ptr)
 	s := C.GoString(p)
 	C.free(unsafe.Pointer(p))
 	return s
 }
 
-func (g Gid) getNumPieces() int {
+func (g Gid) GetNumPieces() int {
 	return int(C.getNumPieces_gid(g.ptr))
 }
 
-func (g Gid) getConnections() int {
+func (g Gid) GetConnections() int {
 	return int(C.getConnections_gid(g.ptr))
 }
 
-func (g Gid) getErrorCode() int {
+func (g Gid) GetErrorCode() int {
 	return int(C.getErrorCode_gid(g.ptr))
 }
 
-func (g Gid) getNumFiles() int {
+func (g Gid) GetNumFiles() int {
 	return int(C.getNumFiles_gid(g.ptr))
 }
 
-func (g Gid) getDir() string {
+func (g Gid) GetDir() string {
 	p := C.getDir_gid(g.ptr)
 	s := C.GoString(p)
 	C.free(unsafe.Pointer(p))
 	return s
 }
 
-func (g Gid) getBtMetaInfo() BtMetaInfoData {
+func (g Gid) GetBtMetaInfo() BtMetaInfoData {
 	var btMetaInfo BtMetaInfoData
 	btmi := C.getBtMetaInfo_gid(g.ptr)
 	if btmi == nil {
-		btMetaInfo.valid = false
+		btMetaInfo.Valid = false
 	} else {
 		//Comment
 		c := C.get_comment_BtMetaInfo(btmi)
-		btMetaInfo.comment = C.GoString(c)
+		btMetaInfo.Comment = C.GoString(c)
 		C.free(unsafe.Pointer(c))
 		//Creation Time
-		btMetaInfo.creationDate = time.Unix(int64(C.get_creationDate_BtMetaInfo(btmi)), 0)
+		btMetaInfo.CreationDate = time.Unix(int64(C.get_creationDate_BtMetaInfo(btmi)), 0)
 		//Mode
 		mode := int(C.get_mode_BtMetaInfo(btmi))
-		btMetaInfo.singleMod = false
-		btMetaInfo.multiMod = false
+		btMetaInfo.SingleMod = false
+		btMetaInfo.MultiMod = false
 		if mode == 0 {
-			btMetaInfo.singleMod = true
+			btMetaInfo.SingleMod = true
 		} else if mode == 1 {
-			btMetaInfo.multiMod = true
+			btMetaInfo.MultiMod = true
 		}
 		//Name
 		n := C.get_name_BtMetaInfo(btmi)
-		btMetaInfo.name = C.GoString(n)
+		btMetaInfo.Name = C.GoString(n)
 		C.free(unsafe.Pointer(n))
 
 		C.free(btmi)
@@ -331,23 +331,23 @@ func (g Gid) getBtMetaInfo() BtMetaInfoData {
 	return btMetaInfo
 }
 
-func (g Gid) getFiles() []FileData {
+func (g Gid) GetFiles() []FileData {
 	var files []FileData
 	var ptr unsafe.Pointer
 	l := int(C.getFiles_gid(g.ptr))
 	for i := 0; i < l; i++ {
 		var f FileData
 		ptr = C.get_element_fileData(C.int(i))
-		f.index = int(C.get_index_fileData(ptr))
+		f.Index = int(C.get_index_fileData(ptr))
 		p := C.get_path_fileData(ptr)
-		f.path = C.GoString(p)
+		f.Path = C.GoString(p)
 		C.free(unsafe.Pointer(p))
-		f.length = int64(C.get_length_fileData(ptr))
-		f.completedLength = int64(C.get_completedLength_fileData(ptr))
+		f.Length = int64(C.get_length_fileData(ptr))
+		f.CompletedLength = int64(C.get_completedLength_fileData(ptr))
 		if int(C.get_selected_fileData(ptr)) == 0 {
-			f.selected = true
+			f.Selected = true
 		} else {
-			f.selected = false
+			f.Selected = false
 		}
 		//Uris not added due to high cost with loops
 		files = append(files, f)
@@ -355,14 +355,14 @@ func (g Gid) getFiles() []FileData {
 	return files
 }
 
-func (d aria2go) getGlobalStat() GlobalStat {
+func (d Aria2go) GetGlobalStat() GlobalStat {
 	var globalStat GlobalStat
 	gs := C.getGlobalStat_aria2go()
-	globalStat.downloadSpeed = int(C.get_downloadSpeed_globalStat(gs))
-	globalStat.uploadSpeed = int(C.get_uploadSpeed_globalStat(gs))
-	globalStat.numActive = int(C.get_numActive_globalStat(gs))
-	globalStat.numWaiting = int(C.get_numWaiting_globalStat(gs))
-	globalStat.numStopped = int(C.get_numStopped_globalStat(gs))
+	globalStat.DownloadSpeed = int(C.get_downloadSpeed_globalStat(gs))
+	globalStat.UploadSpeed = int(C.get_uploadSpeed_globalStat(gs))
+	globalStat.NumActive = int(C.get_numActive_globalStat(gs))
+	globalStat.NumWaiting = int(C.get_numWaiting_globalStat(gs))
+	globalStat.NumStopped = int(C.get_numStopped_globalStat(gs))
 	C.free(gs)
 	return globalStat
 }
--- aria2go_test.go
+++ aria2go_test.go
@@ -13,7 +13,7 @@ import (
 	"gotest.tools/assert"
 )
 
-var downloader aria2go
+var downloader Aria2go
 var gid Gid
 var gid_position Gid
 var gid_to_pause Gid
@@ -43,9 +43,9 @@ const file_link_4 string = "https://www.w3.org/History/1989/proposal-magnify.gif
 
 func TestMain(m *testing.M) {
 	downloader = New()
-	downloader.setEventCallback(func(event DownloadEvent, g Gid) {
+	downloader.SetEventCallback(func(event DownloadEvent, g Gid) {
 		fmt.Printf("Callback Called:\n")
-		s := g.getStatus()
+		s := g.GetStatus()
 		switch event {
 		case EVENT_ON_DOWNLOAD_START:
 			fmt.Printf("Download Start: %s\n", s)
@@ -68,7 +68,7 @@ func TestMain(m *testing.M) {
 		}
 	})
 	ret := m.Run()
-	downloader.finalize()
+	downloader.Finalize()
 	//Delete downloaded files
 	os.Remove(file_path_1)
 	os.Remove(file_path_1_1)
@@ -82,55 +82,55 @@ func TestAll(t *testing.T) {
 	t.Run("Check if function calls protected against nullptr", func(t *testing.T) {
 		//Since session not initilized, expected case is nothing to be happen
 		//If test crashes on this part check pointers and add protection
-		downloader.addUri(file_link_1)
-		g := downloader.addUriInPosition(file_link_2, 0)
-		downloader.isNull(g)
-		h := downloader.gidToHex(g)
-		downloader.hexToGid(h)
-		downloader.getActiveDownload()
-		downloader.removeDownload(g)
-		downloader.forceRemoveDownload(g)
-		downloader.pauseDownload(g)
-		downloader.unpauseDownload(g)
-		downloader.forcePauseDownload(g)
-		downloader.unpauseDownload(g)
+		downloader.AddUri(file_link_1)
+		g := downloader.AddUriInPosition(file_link_2, 0)
+		downloader.IsNull(g)
+		h := downloader.GidToHex(g)
+		downloader.HexToGid(h)
+		downloader.GetActiveDownload()
+		downloader.RemoveDownload(g)
+		downloader.ForceRemoveDownload(g)
+		downloader.PauseDownload(g)
+		downloader.UnpauseDownload(g)
+		downloader.ForcePauseDownload(g)
+		downloader.UnpauseDownload(g)
 	})
 	t.Run("Init Session", func(t *testing.T) {
-		downloader.init_aria2go_session(true)
+		downloader.Init_aria2go_session(true)
 	})
 	t.Run("Init Session Second time", func(t *testing.T) {
-		downloader.init_aria2go_session(false)
+		downloader.Init_aria2go_session(false)
 	})
 	t.Run("Run", func(t *testing.T) {
-		downloader.keepRunning()
+		downloader.KeepRunning()
 	})
 	t.Run("Add uri", func(t *testing.T) {
-		gid = downloader.addUri(file_link_1)
+		gid = downloader.AddUri(file_link_1)
 		time.Sleep(2 * time.Second)
 	})
 	t.Run("Add uri in position", func(t *testing.T) {
-		gid_position = downloader.addUriInPosition(file_link_2, 0)
+		gid_position = downloader.AddUriInPosition(file_link_2, 0)
 		time.Sleep(2 * time.Second)
 		//Todo add get current position and check it
 	})
 	t.Run("is gid null", func(t *testing.T) {
-		assert.Equal(t, downloader.isNull(gid), false)
+		assert.Equal(t, downloader.IsNull(gid), false)
 	})
 	t.Run("gid to hex", func(t *testing.T) {
-		downloader.gidToHex(gid)
+		downloader.GidToHex(gid)
 		//hex can be anything no way to check
 	})
 	t.Run("hex to gid", func(t *testing.T) {
-		hex := downloader.gidToHex(gid)
-		gid_converted := downloader.hexToGid(hex)
+		hex := downloader.GidToHex(gid)
+		gid_converted := downloader.HexToGid(hex)
 		assert.Equal(t, gid, gid_converted)
 	})
 	t.Run("Check Status in position", func(t *testing.T) {
-		sp := gid_position.getStatus()
+		sp := gid_position.GetStatus()
 		assert.Equal(t, sp, DOWNLOAD_COMPLETE)
 	})
 	t.Run("Check Status", func(t *testing.T) {
-		sp := gid.getStatus()
+		sp := gid.GetStatus()
 		assert.Equal(t, sp, DOWNLOAD_COMPLETE)
 	})
 	t.Run("Check Event Counts", func(t *testing.T) {
@@ -150,15 +150,15 @@ func TestAll(t *testing.T) {
 		assert.Equal(t, md5, file_md5_2)
 	})
 	t.Run("Uri Cache", func(t *testing.T) {
-		downloader.addUriToCache("Test")
+		downloader.AddUriToCache("Test")
 	})
 	t.Run("Claer Uri Cache", func(t *testing.T) {
-		downloader.clearUriCache()
+		downloader.ClearUriCache()
 	})
 	t.Run("Add All From Cache", func(t *testing.T) {
-		downloader.addUriToCache(file_link_3)
-		downloader.addUriToCache(file_link_4) //This file will not be downloaded because it does not point to the same file
-		gid = downloader.addAllFromCache()
+		downloader.AddUriToCache(file_link_3)
+		downloader.AddUriToCache(file_link_4) //This file will not be downloaded because it does not point to the same file
+		gid = downloader.AddAllFromCache()
 		time.Sleep(2 * time.Second)
 	})
 	t.Run("Check Downloaded Files From Cache MD5", func(t *testing.T) {
@@ -173,28 +173,28 @@ func TestAll(t *testing.T) {
 		}
 	})
 	t.Run("Get Active Download", func(t *testing.T) {
-		downloader.getActiveDownload()
+		downloader.GetActiveDownload()
 	})
 	t.Run("Remove Download", func(t *testing.T) {
-		downloader.removeDownload(gid)
+		downloader.RemoveDownload(gid)
 		//TODO check events
 	})
 	t.Run("Remove Download Force", func(t *testing.T) {
-		downloader.forceRemoveDownload(gid)
+		downloader.ForceRemoveDownload(gid)
 	})
 	t.Run("Pause Download", func(t *testing.T) {
-		gid_to_pause = downloader.addUri(file_link_1)
-		downloader.pauseDownload(gid_to_pause)
+		gid_to_pause = downloader.AddUri(file_link_1)
+		downloader.PauseDownload(gid_to_pause)
 	})
 	t.Run("Unpause Download", func(t *testing.T) {
-		downloader.unpauseDownload(gid_to_pause)
+		downloader.UnpauseDownload(gid_to_pause)
 	})
 	t.Run("Force Pause Download", func(t *testing.T) {
-		gid_to_pause = downloader.addUri(file_link_1)
-		downloader.forcePauseDownload(gid_to_pause)
+		gid_to_pause = downloader.AddUri(file_link_1)
+		downloader.ForcePauseDownload(gid_to_pause)
 	})
 	t.Run("Unpause Force Paused Download", func(t *testing.T) {
-		downloader.unpauseDownload(gid_to_pause)
+		downloader.UnpauseDownload(gid_to_pause)
 		time.Sleep(2 * time.Second)
 		md5, err := hash_file_md5(file_path_1_1)
 		if err != nil {
@@ -203,33 +203,33 @@ func TestAll(t *testing.T) {
 		assert.Equal(t, file_md5_1, md5)
 	})
 	t.Run("Gid Functions", func(t *testing.T) {
-		gid = downloader.addUri(file_link_4)
+		gid = downloader.AddUri(file_link_4)
 		//TODO find a compatible file to test these functions, and add assert
-		gid.getStatus()
-		gid.getTotalLength()
-		gid.getBitfield()
-		gid.getDownloadSpeed()
-		gid.getUploadSpeed()
-		gid.getInfoHash()
-		gid.getNumPieces()
-		gid.getConnections()
-		gid.getErrorCode()
-		gid.getNumFiles()
+		gid.GetStatus()
+		gid.GetTotalLength()
+		gid.GetBitfield()
+		gid.GetDownloadSpeed()
+		gid.GetUploadSpeed()
+		gid.GetInfoHash()
+		gid.GetNumPieces()
+		gid.GetConnections()
+		gid.GetErrorCode()
+		gid.GetNumFiles()
 		time.Sleep(2 * time.Second)
 	})
 	t.Run("File Data Path", func(t *testing.T) {
-		files := gid.getFiles()
-		s := strings.Split(files[0].path, "/")
+		files := gid.GetFiles()
+		s := strings.Split(files[0].Path, "/")
 		assert.Equal(t, file_path_4, s[len(s)-1])
 	})
 	t.Run("Global Stats", func(t *testing.T) {
-		gs := downloader.getGlobalStat()
-		assert.Equal(t, 6, gs.numStopped)
+		gs := downloader.GetGlobalStat()
+		assert.Equal(t, 6, gs.NumStopped)
 	})
 	t.Run("BTMI basic", func(t *testing.T) {
 		//TODO add a torrent file and do other tests
-		btmi := gid.getBtMetaInfo()
-		assert.Equal(t, btmi.valid, false)
+		btmi := gid.GetBtMetaInfo()
+		assert.Equal(t, btmi.Valid, false)
 	})
 }
 
